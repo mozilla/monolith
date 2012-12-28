36eb493865050c0a4abdfd5f8490622fccdbf3f1
Andy McKay <amckay@mozilla.com>
2012-12-28 15:50:11 -0800

    update to work with jenkins

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -16,7 +16,7 @@ find . -name '*.pyc' -exec rm {} \;
 
 if [ ! -d "$VENV/bin" ]; then
   echo "No virtualenv found.  Making one..."
-  virtualenv $VENV --no-site-packages
+  virtualenv $VENV --system-site-packages
   source $VENV/bin/activate
   pip install --upgrade pip
   pip install coverage
@@ -25,19 +25,15 @@ fi
 git submodule sync -q
 git submodule update --init --recursive
 
-if [ ! -d "$WORKSPACE/vendor" ]; then
-    echo "No /vendor... crap."
-    exit 1
-fi
-
 source $VENV/bin/activate
-pip install -q -r requirements/compiled.txt
-pip install -q -r requirements/dev.txt
+pip install -q --exists-action=w --no-deps -r requirements/test.txt
 
-cat > settings/local.py <<SETTINGS
-from settings.base import *
+cat > solitude/settings/local.py <<SETTINGS
+from solitude.settings.base import *
 
-ROOT_URLCONF = 'workspace.urls'
+DEBUG = True
+SECRET_KEY = 'not-blank-honest'
+ROOT_URLCONF = 'monolith.urls'
 LOG_LEVEL = logging.ERROR
 # Database name has to be set because of sphinx
 DATABASES = {
@@ -54,6 +50,12 @@ DATABASES = {
     }
 }
 
+HMAC_KEYS = {  # for bcrypt only
+    '2011-01-01': 'cheesecake',
+}
+from django_sha2 import get_password_hashers
+PASSWORD_HASHERS = get_password_hashers(BASE_PASSWORD_HASHERS, HMAC_KEYS)
+
 INSTALLED_APPS += ('django_nose',)
 CELERY_ALWAYS_EAGER = True
 SETTINGS
@@ -63,7 +65,8 @@ echo "CREATE DATABASE IF NOT EXISTS ${JOB_NAME}"|mysql -u $DB_USER -h $DB_HOST
 
 echo "Starting tests..."
 export FORCE_DB=1
-coverage run manage.py test --noinput --with-xunit
+coverage run manage.py test --noinput --with-xunit --with-blockage
 coverage xml $(find apps lib -name '*.py')
 
 echo "FIN"
+
