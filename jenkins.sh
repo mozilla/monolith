f7a57a5ebb4630e5b8b36cea9ad8b6153a1f4577
Hanno Schlichting <hanno@hannosch.eu>
2013-02-19 18:17:55 +0100

    add jenkins driver

---
+++ jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+echo "Starting build on executor $EXECUTOR_NUMBER..."
+
+# Make sure there's no old pyc files around.
+find . -name '*.pyc' -exec rm {} \;
+
+make build
+
+echo "Starting tests..."
+
+bin/nosetests -s -d -v --with-xunit --with-coverage --cover-package monolith monolith
+bin/coverage xml $(find monolith -name '*.py')
+
+echo "FIN"
