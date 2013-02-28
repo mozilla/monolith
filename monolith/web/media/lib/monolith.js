874b76cd69a45e4028113d42290869d17be6e4f6
Tarek Ziade <tarek@ziade.org>
2013-02-28 13:57:13 +0000

    display the count

--- monolith/web/media/lib/monolith.js
+++ monolith/web/media/lib/monolith.js
@@ -341,7 +341,13 @@ MonolithBase.extend("MonolithAggregate",
 
       // XXX display the day, week or month in the label...
       $.each(json.facets.facet_histo.entries, function(i, item) {
-        var line = {x: x, y: parseInt(item.total), date: item.time};
+        // XXX why count vs total ?
+        if (item.hasOwnProperty("count")) {
+            var line = {x: x, y: parseInt(item.count), date: item.time};  
+        }
+        else {
+            var line = {x: x, y: parseInt(item.total), date: item.time};
+        }
         data.push(line);
         x += 1;
       });
